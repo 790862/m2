83ba6f4b91fcd46ab97eefa50885e9ad4dd78e8e
790862 <790862@seq.org>
2016-08-31 12:29:38 -0700

    Add files via upload

---
+++ maze/MazeWalker.java
@@ -0,0 +1,19 @@
+ 
+/**
+ * Write a description of class MazeWalker here.
+ * 
+ * @Jesus Aguilar
+ * @08/1\
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+    }
+}
---
+++ maze/MyFirstProgram.java
@@ -0,0 +1,16 @@
+
+/**
+ * Jesus Aguilar
+ * 26 August 2016
+ */
+public class MyFirstProgram
+{
+    public static void run()
+    {
+        Maze maze = new Maze();
+        MazeWalker mazeWalker = new MazeWalker();
+        mazeWalker.walkMaze(maze.getMazeBot());  
+        mazebot
+        
+    }
+}
